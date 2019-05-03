166d5fbe1f0c270ee9ce12671c0909994bd10aeb
Richard Dohn <richard.dohn@7factor.io>
2019-05-03 16:21:57 -0400

    adding ses password

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "smtp_user_key" {
+output "smtp_user_access_id" {
   description = "smtp user access key id"
   value       = "${aws_iam_access_key.smtp_user_access_key.id}"
 }
 
-output "smtp_user_key" {
+output "smtp_user_secret" {
   description = "smtp user secret"
-  value       = "${aws_iam_access_key.smtp_user_access_key.secret}"
+  value       = "${aws_iam_access_key.smtp_user_access_key.ses_smtp_password}"
 }
