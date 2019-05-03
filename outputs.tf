b1dbf9d0439a5cf4f1a3ebd953ed2eac766de799
Richard Dohn <richard.dohn@7factor.io>
2019-05-03 16:18:56 -0400

    adding access key id

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "smtp_user_key" {
   description = "smtp user access key id"
-  value       = "${aws_iam_access_key.smtp_user_access_key}"
+  value       = "${aws_iam_access_key.smtp_user_access_key.id}"
 }
 
 output "smtp_user_key" {
