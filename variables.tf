9cfa9b33e6ba5255f10c6abc51a93a4c546255e0
Richard Dohn <richard.dohn@7factor.io>
2019-07-10 15:43:12 -0400

    updating syntax for new AWS provider 2.7

--- variables.tf
+++ variables.tf
@@ -1,41 +1,41 @@
 variable "dmarc_rua" {
   description = "Email address for capturing DMARC aggregate reports."
-  type        = "string"
+  type        = string
 }
 
 variable "domain_name" {
   description = "The domain name to configure SES."
-  type        = "string"
+  type        = string
 }
 
 variable "enable_verification" {
   description = "Control whether or not to verify SES DNS records."
-  type        = "string"
+  type        = string
   default     = true
 }
 
 variable "from_addresses" {
   description = "List of email addresses to catch bounces and rejections"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "mail_from_domain" {
   description = " Subdomain (of the route53 zone) which is to be used as MAIL FROM address"
-  type        = "string"
+  type        = string
 }
 
 variable "route53_zone_id" {
   description = "Route53 host zone ID to enable SES."
-  type        = "string"
+  type        = string
 }
 
 variable "mx_receive_records" {
   description = "Route53 MX record which defines vaild mail servers."
-  type        = "list"
+  type        = list(string)
 }
 
 variable "spf_records" {
   description = "Route53 MX record which defines vaild mail servers."
-  type        = "list"
+  type        = list(string)
   default     = ["v=spf1 include:amazonses.com -all"]
 }
