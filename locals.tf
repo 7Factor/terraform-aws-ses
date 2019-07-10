649138ebc3ad7a244c3e5cb5540a6aa0a4e4f152
chris-malloy <chrismalloymusic@gmail.com>
2019-07-10 08:17:01 -0400

    Upgrading to terraform 0.12.

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  stripped_domain_name      = "${replace(var.domain_name, "/[.]$/", "")}"
-  stripped_mail_from_domain = "${replace(var.mail_from_domain, "/[.]$/", "")}"
-  dash_domain               = "${replace(var.domain_name, ".", "-")}"
+  stripped_domain_name      = replace(var.domain_name, "/[.]$/", "")
+  stripped_mail_from_domain = replace(var.mail_from_domain, "/[.]$/", "")
+  dash_domain               = replace(var.domain_name, ".", "-")
 }
