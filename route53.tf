c7b8ec1d92bb00d2023b74d3eebbf5a8016a4cbd
onegronm <omarnegron92@hotmail.com>
2022-07-14 15:02:16 -0400

    removed mx record creation

--- route53.tf
+++ route53.tf
@@ -7,30 +7,6 @@ resource "aws_route53_record" "dkim" {
   records = ["${element(aws_ses_domain_dkim.dkim.dkim_tokens, count.index)}.dkim.amazonses.com"]
 }
 
-resource "aws_route53_record" "spf_records" {
-  zone_id = var.route53_zone_id
-  name    = aws_ses_domain_mail_from.mail_from.mail_from_domain
-  type    = "TXT"
-  ttl     = "600"
-  records = var.spf_records
-}
-
-resource "aws_route53_record" "mx_records" {
-  zone_id = var.route53_zone_id
-  name    = aws_ses_domain_mail_from.mail_from.mail_from_domain
-  type    = "MX"
-  ttl     = "600"
-  records = var.mx_records
-}
-
-resource "aws_route53_record" "dmarc" {
-  zone_id = var.route53_zone_id
-  name    = "_dmarc.${var.domain_name}"
-  type    = "TXT"
-  ttl     = "600"
-  records = ["v=DMARC1; p=none; rua=mailto:${var.dmarc_email};"]
-}
-
 resource "aws_route53_record" "ses_verification" {
   zone_id = var.route53_zone_id
   name    = "_amazonses.${aws_ses_domain_identity.domain_id.id}"
--- variables.tf
+++ variables.tf
@@ -9,15 +9,3 @@ variable "domain_name" {
 variable "route53_zone_id" {
   description = "Route53 host zone ID to enable SES."
 }
-
-variable "mx_records" {
-  type        = list(string)
-  default     = ["10 inbound-smtp.us-east-1.amazonaws.com"]
-  description = "Route53 MX record which defines vaild mail servers. Defaults to `10 inbound-smtp.us-east-1.amazonaws.com`."
-}
-
-variable "spf_records" {
-  type        = list(string)
-  default     = ["v=spf1 include:amazonses.com -all"]
-  description = "Route53 MX record which defines valid mail servers. Defaults to `v=spf1 include:amazonses.com -all`"
-}
