d9b4410f2dd3fb04b81dc69bdd0379f12e58fa46
onegronm <omarnegron92@hotmail.com>
2022-07-14 15:06:08 -0400

    removed unused variable

--- variables.tf
+++ variables.tf
@@ -1,7 +1,3 @@
-variable "dmarc_email" {
-  description = "Email address for capturing DMARC aggregate reports."
-}
-
 variable "domain_name" {
   description = "The domain name to configure SES. Must be a top level, FQDN."
 }
